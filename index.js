badd5c42f7e1553b932f2835b7e2d50c7ef40df7
vamshi <alugojuvamshi@gmail.com>
2023-04-16 21:40:12 +0530

    async way of doing

--- index.js
+++ index.js
@@ -8,48 +8,49 @@ let TotalPrice = document.querySelector("#Total_value");
 document.querySelector("#submit_btn").addEventListener("click", addItem);
 window.addEventListener("load", LoadFirst);
 
-function LoadFirst() {
-  axios
-    .get("https://crudcrud.com/api/9923a9c77a3f4184bc6e38f16c6aa4f6/products")
-    .then((res) => {
-      res.data.forEach((item) => {
-        // localStorage.setItem(item._id, JSON.stringify(item));
-        Tprice += parseInt(item.price);
-        addToDom(item);
-      });
-    })
-    .catch((err) => {
-      console.log(err);
+
+// to get the items from the crud crud api;
+async function LoadFirst() {
+  try {
+    let res = await axios.get(
+      "https://crudcrud.com/api/9923a9c77a3f4184bc6e38f16c6aa4f6/products"
+    );
+    res.data.forEach((item) => {
+      Tprice += parseInt(item.price);
+      addToDom(item);
     });
+  } catch {}
 }
 
-function addItem(e) {
+
+// invoked when a new item is added
+async function addItem(e) {
   e.preventDefault();
   let obj = {
     name: InpName.value,
     price: InpPrice.value,
   };
 
-  axios
-    .post(
+  try {
+    await axios.post(
       "https://crudcrud.com/api/9923a9c77a3f4184bc6e38f16c6aa4f6/products",
       obj
-    )
-    .then((res) => {
-      localStorage.setItem(res.data._id, JSON.stringify(res.data));
-      Tprice = Tprice + parseInt(res.data.price);
-      addToDom(res.data);
-    });
+    );
+
+    Tprice += parseInt(obj.price);
+    addToDom(obj);
+  } catch {}
 }
 
+
+// function that adds items to dom
 function addToDom(obj) {
-  // let obj = JSON.parse(localStorage.getItem(Id));
   let newli = document.createElement("li");
   let newbtn = document.createElement("button");
- 
+
   let span = document.createElement("span");
   newli.textContent = obj.name + " - ";
-  span.textContent =  obj.price+" ";
+  span.textContent = obj.price + " ";
   span.id = "Price";
   newli.classList = "list-group-item";
   newli.id = obj._id;
@@ -57,10 +58,10 @@ function addToDom(obj) {
   newbtn.classList = "btn btn-danger btn-sm";
   newbtn.id = "delete_btn";
   newbtn.addEventListener("click", deleteList);
-  
-  newli.appendChild(span)
+
+  newli.appendChild(span);
   newli.appendChild(newbtn);
-  
+
   ListItems.appendChild(newli);
 
   console.log(Tprice);
@@ -68,24 +69,18 @@ function addToDom(obj) {
 }
 
 
-function deleteList(e) {
+// function to remove the element 
+async function deleteList(e) {
   let parentLi = e.target.parentElement;
   console.log(parentLi);
   let ObjId = parentLi.getAttribute("id");
-  // Tprice -= parseInt(JSON.parse(localStorage.getItem(ObjId)).price);
-  // localStorage.removeItem(ObjId);
-  Tprice -=  parseInt(parentLi.querySelector("#Price").innerText);
+  Tprice -= parseInt(parentLi.querySelector("#Price").innerText);
   ListItems.removeChild(parentLi);
-  axios
-    .delete(
-      `https://crudcrud.com/api/9923a9c77a3f4184bc6e38f16c6aa4f6/products/${ObjId}`
-    )
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-
   TotalPrice.innerHTML = Tprice;
+
+  try {
+    await axios.delete(
+      `https://crudcrud.com/api/9923a9c77a3f4184bc6e38f16c6aa4f6/products/${ObjId}`
+    );
+  } catch {}
 }
